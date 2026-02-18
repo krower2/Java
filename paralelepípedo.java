73e607ff52d3bbddc03ac2cde59567040f30f8a6
krower2 <123036135+krower2@users.noreply.github.com>
2026-02-18 15:28:16 -0600

    Create  paralelepípedo.java

---
+++ paralelepípedo.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese los valores del primer vector:");
+        Vector u = leerVector(sc);
+
+        System.out.println("Ingrese los valores del segundo vector:");
+        Vector v = leerVector(sc);
+
+        System.out.println("Ingrese los valores del tercer vector:");
+        Vector w = leerVector(sc);
+
+        operaciones p = new operaciones(u, v, w);
+
+        int opcion;
+
+        do {
+            System.out.println("\nMENU");
+            System.out.println("1. Mostrar informacion");
+            System.out.println("2. Calcular volumen");
+            System.out.println("3. Modificar vectores");
+            System.out.println("4. Salir");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println(p);
+                    break;
+
+                case 2:
+                    System.out.println("Volumen: " + p.calcularVolumen());
+                    break;
+
+                case 3:
+                    modificarVectores(sc, p);
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida");
+            }
+
+        } while (opcion != 4);
+
+        sc.close();
+    }
+
+    public static Vector leerVector(Scanner sc) {
+        System.out.print("x: ");
+        double x = sc.nextDouble();
+        System.out.print("y: ");
+        double y = sc.nextDouble();
+        System.out.print("z: ");
+        double z = sc.nextDouble();
+        return new Vector(x, y, z);
+    }
+
+    public static void modificarVectores(Scanner sc, operaciones p) {
+
+        int op;
+
+        do {
+            System.out.println("\nSUBMENU");
+            System.out.println("1. Modificar primer vector");
+            System.out.println("2. Modificar segundo vector");
+            System.out.println("3. Modificar tercer vector");
+            System.out.println("4. Regresar");
+            op = sc.nextInt();
+
+            switch (op) {
+
+                case 1:
+                    p.setU(leerVector(sc));
+                    break;
+
+                case 2:
+                    p.setV(leerVector(sc));
+                    break;
+
+                case 3:
+                    p.setW(leerVector(sc));
+                    break;
+
+            }
+
+        } while (op != 4);
+    }
+}

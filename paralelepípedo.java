9cce82e9c74987f65ad81d413075e7effe5f73e2
krower2 <123036135+krower2@users.noreply.github.com>
2026-02-20 09:58:19 -0600

    Refactor Paralelepipedo class and remove main method

--- paralelepípedo.java
+++ paralelepípedo.java
@@ -1,103 +1,55 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package operaciones;
 
-import java.util.Scanner;
+public class Paralelepipedo {
 
-public class Main {
+    private Vector u;
+    private Vector v;
+    private Vector w;
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Ingrese los valores del primer vector:");
-        Vector u = leerVector(sc);
-
-        System.out.println("Ingrese los valores del segundo vector:");
-        Vector v = leerVector(sc);
-
-        System.out.println("Ingrese los valores del tercer vector:");
-        Vector w = leerVector(sc);
-
-        operaciones p = new operaciones(u, v, w);
-
-        int opcion;
-
-        do {
-            System.out.println("\nMENU");
-            System.out.println("1. Mostrar informacion");
-            System.out.println("2. Calcular volumen");
-            System.out.println("3. Modificar vectores");
-            System.out.println("4. Salir");
-            opcion = sc.nextInt();
-
-            switch (opcion) {
-
-                case 1:
-                    System.out.println(p);
-                    break;
-
-                case 2:
-                    System.out.println("Volumen: " + p.calcularVolumen());
-                    break;
-
-                case 3:
-                    modificarVectores(sc, p);
-                    break;
-
-                case 4:
-                    System.out.println("Saliendo...");
-                    break;
-
-                default:
-                    System.out.println("Opcion invalida");
-            }
-
-        } while (opcion != 4);
-
-        sc.close();
+    public Paralelepipedo(Vector u, Vector v, Vector w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
     }
 
-    public static Vector leerVector(Scanner sc) {
-        System.out.print("x: ");
-        double x = sc.nextDouble();
-        System.out.print("y: ");
-        double y = sc.nextDouble();
-        System.out.print("z: ");
-        double z = sc.nextDouble();
-        return new Vector(x, y, z);
+    public Vector getU() {
+        return u;
     }
 
-    public static void modificarVectores(Scanner sc, operaciones p) {
+    public void setU(Vector u) {
+        this.u = u;
+    }
 
-        int op;
+    public Vector getV() {
+        return v;
+    }
 
-        do {
-            System.out.println("\nSUBMENU");
-            System.out.println("1. Modificar primer vector");
-            System.out.println("2. Modificar segundo vector");
-            System.out.println("3. Modificar tercer vector");
-            System.out.println("4. Regresar");
-            op = sc.nextInt();
+    public void setV(Vector v) {
+        this.v = v;
+    }
 
-            switch (op) {
+    public Vector getW() {
+        return w;
+    }
 
-                case 1:
-                    p.setU(leerVector(sc));
-                    break;
+    public void setW(Vector w) {
+        this.w = w;
+    }
 
-                case 2:
-                    p.setV(leerVector(sc));
-                    break;
+    public double calcularVolumen() {
+        Vector cruz = u.productoCruz(v);
+        double triple = w.productoPunto(cruz);
+        return Math.abs(triple);
+    }
 
-                case 3:
-                    p.setW(leerVector(sc));
-                    break;
-
-            }
-
-        } while (op != 4);
+    @Override
+    public String toString() {
+        return "Vector U: " + u +
+               "\nVector V: " + v +
+               "\nVector W: " + w;
     }
 }
